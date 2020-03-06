3537305bf0bce9de3235c8647c471b2113167961
Starinets <tsn.ipad@gmail.com>
2020-03-06 07:38:23 +0600

    Окончание

--- js/modal.js
+++ js/modal.js
@@ -1,3 +1,99 @@
-document.onload = function () {
-   let modalCloseButton = document.querySelectorAll('.close-modal-button');
+var writeAsModal = document.querySelector(".write-us-modal");
+var writeAsButtonShow = document.querySelector(".write-us-show");
+var writeAsButtonClose = document.querySelector(".write-as-modal-close");
+var writeAsButtonSend = document.querySelector(".write-as-modal-submit");
+
+var mapModal = document.querySelector(".map-modal");
+var mapButtonShow = document.querySelector(".map-show");
+var mapButtonClose = document.querySelector(".map-modal-close");
+
+var writeAsForm = writeAsModal.querySelector("form");
+var writeAsUserName = writeAsModal.querySelector("#writetous-name");
+var writeAsUserEmail = writeAsModal.querySelector("#writetous-email");
+var writeAsUserMessage = writeAsModal.querySelector("#writetous-boby");
+
+var isStorageSupport = true;
+var storageUserName = "";
+var storageUserEmail = "";
+
+try {
+   storageName = localStorage.getItem("user-name");
+   storageEmail = localStorage.getItem("user-email");
+} catch (err) {
+   isStorageSupport = false;
 }
+
+writeAsButtonShow.addEventListener("click", function (evt) {
+   evt.preventDefault();
+   writeAsModal.classList.add("modal-show");
+   if (storageUserName || storageUserEmail) {
+      if (storageUserName) {
+         writeAsUserName.value = storageUserName;
+         writeAsUserName.focus();
+      }
+      if (storageUserEmail) {
+         writeAsUserEmail.value = storageUserEmail;
+         writeAsUserEmail.focus();
+      }
+   } else {
+      writeAsUserName.focus();
+   }
+});
+
+writeAsButtonClose.addEventListener("click", function (evt) {
+   evt.preventDefault();
+   writeAsModal.classList.remove("modal-error");
+   writeAsModal.classList.add("modal-hide");
+   setTimeout(function () {
+      writeAsModal.classList.remove("modal-hide");
+      writeAsModal.classList.remove("modal-show");
+   }, 600);
+});
+
+writeAsForm.addEventListener("submit", function (evt) {
+   if (!writeAsUserName.value || !writeAsUserEmail.value) {
+      evt.preventDefault();
+      writeAsModal.classList.remove("modal-error");
+      writeAsModal.offsetWidth = writeAsModal.offsetWidth;
+      writeAsModal.classList.add("modal-error");
+   } else {
+      if (isStorageSupport) {
+         localStorage.setItem("user-name", writeAsUserName.value);
+         localStorage.setItem("user-email", storageUserEmail.value);
+      }
+   }
+});
+
+window.addEventListener("keydown", function (evt) {
+   if (evt.keyCode === 27) {
+      evt.preventDefault();
+      if (writeAsModal.classList.contains("modal-show")) {
+         writeAsModal.classList.add("modal-hide");
+         setTimeout(function () {
+            writeAsModal.classList.remove("modal-hide");
+            writeAsModal.classList.remove("modal-show");
+         }, 600);
+      }
+      if (mapModal.classList.contains("modal-show")) {
+         mapModal.classList.add("modal-hide");
+         setTimeout(function () {
+            mapModal.classList.remove("modal-hide");
+            mapModal.classList.remove("modal-show");
+         }, 600);
+      }
+   }
+});
+
+mapButtonShow.addEventListener("click", function (evt) {
+   evt.preventDefault();
+   mapModal.classList.add("modal-show");
+});
+
+mapButtonClose.addEventListener("click", function (evt) {
+   evt.preventDefault();
+   mapModal.classList.add("modal-hide");
+   setTimeout(function () {
+      mapModal.classList.remove("modal-hide");
+      mapModal.classList.remove("modal-show");
+   }, 600);
+});
--- js/tabs.js
+++ js/tabs.js
@@ -1,19 +1,16 @@
+var servicesTabs = document.getElementsByClassName("tab-content");
+var servicesTabslinks = document.getElementsByClassName("tablink");
 
 function changeTab(evt, tabID) {
-   var i, x, tablinks;
-   x = document.getElementsByClassName("tab-content");
-   for (i = 0; i < x.length; i++) {
-      x[i].style.display = "none";
-   }
-   tablinks = document.getElementsByClassName("tablink");
-   for (i = 0; i < x.length; i++) {
-      tablinks[i].className = tablinks[i].className.replace(" tablink-active", "");
+   for (let i = 0; i < servicesTabs.length; i++) {
+      servicesTabs[i].style.display = "none";
+      servicesTabslinks[i].className = servicesTabslinks[i].className.replace(" tablink-active", "");
    }
    document.getElementById(tabID).style.display = "block";
    if (evt != '') {
       evt.currentTarget.className += ' tablink-active';
    } else {
-      tablinks[0].className = tablinks[0].className += " tablink-active";
+      servicesTabslinks[0].className = servicesTabslinks[0].className += " tablink-active";
    }
 }
 
